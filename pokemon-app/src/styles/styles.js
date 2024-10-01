1d1f9197208f727b548dd832bf708bb92e052db7
Eduardo Santiago <stgocruzmanuel@gmail.com>
2024-09-30 20:37:35 -0600

    feat(styles): create styles.js

---
+++ pokemon-app/src/styles/styles.js
@@ -0,0 +1,75 @@
+import { css } from 'lit';
+
+export const mainStyles = css`
+    h1, h2, h3, h4, h5, h6, p {
+        margin: 0;
+    }
+
+    .cursor-pointer {
+        cursor: pointer;
+    }
+
+    .w-full {
+        width: 100%;
+    }
+    
+    .d-flex {
+        display: flex;
+    }
+    
+    .d-flex-col {
+        flex-direction: column;
+    }
+    
+    .d-flex-wrap {
+        flex-wrap: wrap;
+    }
+    
+    .d-flex-justify-between {
+        justify-content: space-between;
+    }
+
+    .d-flex-justify-end {
+        justify-content: flex-end
+    }
+    
+    .d-flex-justify-center {
+        justify-content: center
+    }
+    
+    .d-flex-items-center {
+        align-items: center;
+    }
+    
+    .border-box {
+        box-sizing: border-box;
+    }
+    
+    .font-w-100 {
+        font-weight: 100;
+    }
+    
+    .font-w-200 {
+        font-weight: 200;
+    }
+    
+    .font-w-300 {
+        font-weight: 300;
+    }
+    
+    .font-w-400 {
+        font-weight: 400;
+    }
+    
+    .font-w-500 {
+        font-weight: 500;
+    }
+    
+    .font-w-600 {
+        font-weight: 600;
+    }
+    
+    .font-w-700 {
+        font-weight: 700;
+    }
+`;
